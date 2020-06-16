2e40463dfe58b7d1d2c1ade6e75a608c60fad0a0
Nicolasmaidana <brunonicolas.mc@gmail.com>
2020-06-16 18:08:49 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+      
+      <img
+        src="/icono-blanco.svg"
+        className="align-top"
+        alt="tranqui"
+      />
+      </Navbar.Brand>
+    </Navbar>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
